9368308e7959090cf56270ecdbcd5d348472acf8
georgeussher <gussher@icloud.com>
2023-03-04 18:37:53 +0000

    added starTimer function

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-let timeBoxEl = document.getElementById("time-box");
-let timeOn = document.getElementById("elapsed-time");
-let [milliseconds, seconds, minutes, hours] = [0,0,0,0];
-let int = null
+var timeBoxEl = document.getElementById("time-box");
+var appendseconds = document.getElementById("seconds")
+var appendmilliseconds = document.getElementById("milliseconds")
+
+var seconds = 00;
+var milliseconds = 00;
+
 
 function timeCall(){
-  timeBoxEl.innerHTML = Date()
+timeBoxEl.innerHTML = Date()
 }
+
 function startTimer(){
-  if (int !== null){
-    clearInterval(int);
+  milliseconds++ 
+  if (milliseconds < 9){
+    appendmilliseconds.innerHTML = "0" + milliseconds
   }
-  int = setInterval(displayTimer,10);
 }
