f8d697a8d743f1f05e979044facfdb1ca7bf808b
georgeussher <gussher@icloud.com>
2023-03-04 17:48:27 +0000

    started timer function

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let timeBoxEl = document.getElementById("time-box");
+let timeOn = document.getElementById("elapsed-time");
+let [milliseconds, seconds, minutes, hours] = [0,0,0,0];
+let int = null
 
 function timeCall(){
   timeBoxEl.innerHTML = Date()
+}
+function startTimer(){
+  if (int !== null){
+    clearInterval(int);
+  }
+  int = setInterval(displayTimer,10);
 }
