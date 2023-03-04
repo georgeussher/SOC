d9d0f721ad4bb064720b4e1b3e5543642ad3ef3d
georgeussher <gussher@icloud.com>
2023-03-04 19:31:31 +0000

    comment out stopwatch logic

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-var timeBoxEl = document.getElementById("time-box");
-var appendseconds = document.getElementById("seconds")
-var appendmilliseconds = document.getElementById("milliseconds")
-
-var seconds = 00;
-var milliseconds = 00;
-
+let timeBoxEl = document.getElementById("time-box");
+//var appendSeconds = document.getElementById("seconds")
+//var appendMilliseconds = document.getElementById("milliseconds")
+//var buttonStart = document.getElementById("start")
+//var buttonStop = document.getElementById("stop")
+//var buttonReset = document.getElementById("reset")
+//var seconds = 00;
+//var milliseconds = 00;
+//var interval;
 
 function timeCall(){
-timeBoxEl.innerHTML = Date()
+timeBoxEl.textContent = Date()
 }
 
-function startTimer(){
-  milliseconds++ 
-  if (milliseconds < 9){
-    appendmilliseconds.innerHTML = "0" + milliseconds
-  }
-}
+// function startTimer(){
+//   milliseconds++; 
+//   if (milliseconds < 9){
+//     appendMilliseconds.textContent = "0" + milliseconds;
+//   } if (milliseconds > 9){
+//     appendMilliseconds.textContent = milliseconds;
+//   } if (milliseconds > 99){
+//     seconds++;
+//     appendSeconds.textContent = "0" + seconds;
+//     milliseconds = 0;
+//     appendMilliseconds.textContent = "0" + milliseconds;
+//   } if (seconds > 9){
+//     appendSeconds.textContent = seconds;
+//   }
+// }
+//   buttonStart.onclick = function(){
+//    interval = setInterval(startTimer);
+// };
+// console.log(timeBoxEl)
